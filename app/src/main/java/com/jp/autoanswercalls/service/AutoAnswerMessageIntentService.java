a624467da49a7c55192f0bb46a8caf4d01079b89
jp1017 <jp8917@163.com>
2016-10-30 19:56:36 +0800

    bug fix

--- app/src/main/java/com/jp/autoanswercalls/service/AutoAnswerMessageIntentService.java
+++ app/src/main/java/com/jp/autoanswercalls/service/AutoAnswerMessageIntentService.java
@@ -5,7 +5,6 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.database.BulkCursorDescriptor;
 import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.text.TextUtils;
